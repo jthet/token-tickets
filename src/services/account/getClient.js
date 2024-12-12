a455c4dd2579049077eccb0c2f8bf6d41ff46787
Jackson Thetford <j.thetford@utexas.edu>
2024-12-12 00:35:13 -0600

    adding default params to operator client

--- src/services/account/getClient.js
+++ src/services/account/getClient.js
@@ -5,6 +5,9 @@ function getClient(network = 'testnet') {
   const client =
     network === 'testnet' ? Client.forTestnet() : Client.forMainnet();
   client.setOperator(accountId, privateKey);
+  client.setDefaultMaxTransactionFee(new Hbar(100));
+  client.setDefaultMaxQueryPayment(new Hbar(50));
+
   return client;
 }
 
